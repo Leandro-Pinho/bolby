b22d2a17bad8bf3dd70cdb1d07669d5c127f53f2
Leandro-Pinho <leandroessencia@gmail.com>
2023-08-15 10:24:27 -0300

    criando e estilizando a sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
 import "./sidebar.css"
+import Logo from '../../assets/logo.svg'
 
 const sidebar = () => {
     return (
-        <div>
-            sidebar
-        </div>
+        <aside className='aside'>
+            <a href="#home" className='nav__logo'>
+                <img src={Logo} alt="" />
+            </a>
+
+            <nav className='nav'>
+                <div className="nav__menu">
+                    <div className="nav__list">
+                        <ul>
+                            <li className="nav__item">
+                                <a href="#home" className='nav__link'>
+                                    <i className="icon-home"></i>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#about" className='nav__link'>
+                                    <i className="icon-user-following"></i>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#services" className='nav__link'>
+                                    <i className="icon-briefcase"></i>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#resume" className='nav__link'>
+                                    <i className="icon-graduation"></i>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#portfolio" className='nav__link'>
+                                    <i className="icon-layers"></i>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#blog" className='nav__link'>
+                                    <i className="icon-note"></i>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#contact" className='nav__link'>
+                                    <i className="icon-bubble"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div className="nav__footer">
+                <span className="copyright">&copy; 2023 - 2024.</span>
+            </div>
+        </aside>
     )
 }
 
