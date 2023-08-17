32013faa84384a04c9219e8986d2a77d27de6f39
Leandro-Pinho <leandroessencia@gmail.com>
2023-08-17 10:06:46 -0300

    finalizando a criação e estilização da home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import HeaderSocials from './HeaderSocials'
 import "./home.css"
+import Me from '../../assets/avatar-1.svg'
+import ScrollDown from './ScrollDown'
+import Shapes from './Shapes'
 
 const Home = () => {
     return (
-        <div>
-home
-        </div>
+        <section className="home container" id='home'>
+            <div className="intro">
+                <img src={Me} alt="" className='home__img' />
+                <h1 className="home__name">Bolby Doe</h1>
+                <span className="home__education">I'm a Front-End developer</span>
+
+                <HeaderSocials />
+
+                <a href="#contact" className='btn'>Hire Me</a>
+
+                <ScrollDown />
+            </div>
+            <Shapes />
+        </section>
     )
 }
 
