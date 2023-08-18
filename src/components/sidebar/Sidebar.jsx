b61d5d2555fd50f6c64d15565a028df7a97d0f61
Leandro-Pinho <leandroessencia@gmail.com>
2023-08-18 10:59:28 -0300

    criando o active link no sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./sidebar.css"
 import Logo from '../../assets/logo.svg'
 
-const sidebar = () => {
+const Sidebar = () => {
+    const [active, setActive] = useState(1);
+
+    const toggleTab = (index) => {
+        setActive(index)
+    }
+
     return (
         <aside className='aside'>
             <a href="#home" className='nav__logo'>
@@ -14,37 +20,37 @@ const sidebar = () => {
                     <div className="nav__list">
                         <ul>
                             <li className="nav__item">
-                                <a href="#home" className='nav__link'>
+                                <a href="#home" className={active === 1 ? 'nav__link active' : 'nav__link'} onClick={() => toggleTab(1)}>
                                     <i className="icon-home"></i>
                                 </a>
                             </li>
                             <li className="nav__item">
-                                <a href="#about" className='nav__link'>
+                                <a href="#about" className={active === 2 ? 'nav__link active' : 'nav__link'} onClick={() => toggleTab(2)}>
                                     <i className="icon-user-following"></i>
                                 </a>
                             </li>
                             <li className="nav__item">
-                                <a href="#services" className='nav__link'>
+                                <a href="#services" className={active === 3 ? 'nav__link active' : 'nav__link'} onClick={() => toggleTab(3)}>
                                     <i className="icon-briefcase"></i>
                                 </a>
                             </li>
                             <li className="nav__item">
-                                <a href="#resume" className='nav__link'>
+                                <a href="#resume" className={active === 4 ? 'nav__link active' : 'nav__link'} onClick={() => toggleTab(4)}>
                                     <i className="icon-graduation"></i>
                                 </a>
                             </li>
                             <li className="nav__item">
-                                <a href="#portfolio" className='nav__link'>
+                                <a href="#portfolio" className={active === 5 ? 'nav__link active' : 'nav__link'} onClick={() => toggleTab(5)}>
                                     <i className="icon-layers"></i>
                                 </a>
                             </li>
                             <li className="nav__item">
-                                <a href="#blog" className='nav__link'>
+                                <a href="#blog" className={active === 6 ? 'nav__link active' : 'nav__link'} onClick={() => toggleTab(6)}>
                                     <i className="icon-note"></i>
                                 </a>
                             </li>
                             <li className="nav__item">
-                                <a href="#contact" className='nav__link'>
+                                <a href="#contact" className={active === 7 ? 'nav__link active' : 'nav__link'} onClick={() => toggleTab(7)}>
                                     <i className="icon-bubble"></i>
                                 </a>
                             </li>
@@ -60,4 +66,4 @@ const sidebar = () => {
     )
 }
 
-export default sidebar
+export default Sidebar
