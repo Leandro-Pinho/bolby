870f54e67ef63cb06c7949f00f944ed77499b62d
Leandro-Pinho <leandroessencia@gmail.com>
2023-08-19 11:13:35 -0300

    criando o resume e estilizando

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
+import Data from './Data'
+import Card from './Card'
 import "./resume.css"
 
 const Resume = () => {
   return (
-    <div>
-      resume
-    </div>
+    <section className="resume container section" id="resume">
+      <h2 className='section__title'>Experience</h2>
+
+      <div className="resume__container grid">
+        <div className="timeline grid">
+          {Data.map((val, id) => {
+            if (val.category === "education") {
+              return (
+                <Card
+                  key={id}
+                  icon={val.icon}
+                  title={val.title}
+                  year={val.year}
+                  desc={val.desc}
+                />
+              )
+            }
+          })}
+        </div>
+
+        <div className="timeline grid">
+          {Data.map((val, id) => {
+            if (val.category === "experience") {
+              return (
+                <Card
+                  key={id}
+                  icon={val.icon}
+                  title={val.title}
+                  year={val.year}
+                  desc={val.desc}
+                />
+              )
+            }
+          })}
+        </div>
+      </div>
+    </section>
   )
 }
 
